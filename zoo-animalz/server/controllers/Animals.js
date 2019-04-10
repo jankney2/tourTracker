2f6dd4bf07b2f5f6edbdc7e7960cf1a12ac3c5f8
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-04-10 10:27:37 -0600

    created basic server and anmial ctrl

---
+++ zoo-animalz/server/controllers/Animals.js
@@ -0,0 +1,23 @@
+let id = 1
+
+let animals = [
+  {
+    id:id++,
+    species: "gorilla",
+    quantity: 100,
+    imageUrl: "https://media.graytvinc.com/images/810*455/harambe+cincinnati+zoo+gorilla.jpg"
+  },
+  {
+    id:id++,
+    species: "T-Rex",
+    quantity: 1,
+    imageUrl: "https://i.pinimg.com/originals/43/78/59/437859c98eb736d81f5dbdc2cf00cf84.jpg"
+  }
+]
+
+
+
+module.exports= {
+  get: (req, res) => {res.send(animals)},
+
+}
---
+++ zoo-animalz/server/index.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const AnimalCtrl= require('./controllers/Animals')
+const app = express()
+const port= 4040
+app.use(express.json()) //top level middleware- use to have access to req.body
+
+
+
+app.listen(port, ()=> console.log("listening on ", port))
+
+app.get("/api/animals", AnimalCtrl.get)
+
+
