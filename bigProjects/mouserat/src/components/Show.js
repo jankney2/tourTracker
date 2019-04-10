5cb73a6b66269435e996e0546285a90ccc6ee7ff
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-04-10 17:41:24 -0600

    i think i broke github

---
+++ bigProjects/mouserat/src/components/Show.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+import axios from "axios"
+
+export default class Show extends Component {
+
+  constructor(props) {
+    super(props)
+    
+  }
+
+render(){
+  return(
+    <div>Hey boyo</div>
+  )
+}
+
+}
--- bigProjects/mouserat/src/components/ShowList.js
+++ bigProjects/mouserat/src/components/ShowList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-
+import Show from "./Show"
 export default class ShowList extends Component {
 
 
@@ -19,9 +19,14 @@ componentDidMount() {
   ).catch((err)=>console.log("err", err))
 }
 
+
 render() {
   return(
-    <div>Hey there</div>
+    <div>
+      {this.state.shows.map((element)=>{
+        return <Show />
+      })}
+    </div>
   )
 }
 
